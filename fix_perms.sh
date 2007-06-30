3552220d765358e144ef08bab441dd04b702184f
Daniel G. Siegel <dgsiegel@src.gnome.org>
2007-06-30 15:57:43 +0000

    added fix_perms.sh-script in order to have the right permissions on executables
    svn path=/trunk/; revision=53

---
+++ fix_perms.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chmod +x configure
+chmod +x toc2/bin/atsign_parse
+chmod +x toc2/bin/cleanup_vars
+chmod +x toc2/bin/create_makefile_stubs.sh
+chmod +x toc2/bin/create_project_skeleton.sh
+chmod +x toc2/bin/install-sh
+chmod +x toc2/bin/makedist
+chmod +x toc2/bin/removeDupeArgs
+chmod +x toc2/sbin/toc2_core.sh
+chmod +x toc2/sbin/toconfigure
