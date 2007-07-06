a83246223ec1554e625a6bd5b5bfbf875054766f
Daniel G. Siegel <dgsiegel@src.gnome.org>
2007-07-06 22:56:24 +0000

    update license
    svn path=/trunk/; revision=91

--- src/window.c
+++ src/window.c
@@ -47,17 +47,17 @@ void on_about_cb (GtkWidget *p_widget, gpointer user_data)
   };
 
   const char *license[] = {
-    _("This program is free software; you can redistribute it and/or modify "
-      "it under the terms of the GNU General Public License as published by "
-      "the Free Software Foundation; either version 2 of the License, or "
-      "(at your option) any later version.\n"),
-    _("This program is distributed in the hope that it will be useful, "
-      "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-      "GNU General Public License for more details.\n"),
-    _("You should have received a copy of the GNU General Public License "
-      "along with this program; if not, write to the Free Software "
-      "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.")
+    N_("This program is free software; you can redistribute it and/or modify "
+       "it under the terms of the GNU General Public License as published by "
+       "the Free Software Foundation; either version 2 of the License, or "
+       "(at your option) any later version.\n"),
+    N_("This program is distributed in the hope that it will be useful, "
+       "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+       "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+       "GNU General Public License for more details.\n"),
+    N_("You should have received a copy of the GNU General Public License "
+       "along with this program; if not, write to the Free Software "
+       "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.")
   };
 
   char *license_trans;
