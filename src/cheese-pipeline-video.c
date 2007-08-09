ff68aa211756629c5f46aef636b9927767b0b920
Daniel G. Siegel <dgsiegel@src.gnome.org>
2007-08-09 16:58:48 +0000

    add video class
    svn path=/trunk/; revision=135

---
+++ src/cheese-pipeline-video.c
@@ -0,0 +1,307 @@
+/*
+ * Copyright (C) 2007 Copyright (C) 2007 daniel g. siegel <dgsiegel@gmail.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cheese-config.h"
+
+#include <gst/interfaces/xoverlay.h>
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include <libgnomevfs/gnome-vfs.h>
+
+#include "cheese.h"
+#include "cheese-window.h"
+#include "cheese-pipeline-video.h"
+#include "cheese-effects-widget.h"
+#include "cheese-fileutil.h"
+
+G_DEFINE_TYPE(PipelineVideo, cheese_pipeline_video, G_TYPE_OBJECT)
+
+static GObjectClass *parent_class = NULL;
+
+typedef struct _PipelineVideoPrivate PipelineVideoPrivate;
+
+struct _PipelineVideoPrivate
+{
+  int picture_requested;
+
+  GstElement *source;
+  GstElement *ffmpeg1, *ffmpeg2, *ffmpeg3;
+  GstElement *tee;
+  GstElement *queuevid, *queueimg;
+  GstElement *caps;
+  GstElement *effect;
+  GstCaps *filter;
+  GstElement *gst_test_pipeline;
+};
+
+#define PIPELINE_VIDEO_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), PIPELINE_VIDEO_TYPE, PipelineVideoPrivate))
+
+// private methods
+static gboolean cheese_pipeline_video_have_data_cb(GstElement *element, GstBuffer *buffer, GstPad *pad, gpointer user_data);
+static void cheese_pipeline_video_lens_open(PipelineVideo *self);
+static void cheese_pipeline_video_create_video(unsigned char *data, int width, int height);
+
+void
+cheese_pipeline_video_finalize(GObject *object)
+{
+  PipelineVideoPrivate *priv = PIPELINE_VIDEO_GET_PRIVATE(object);
+  gst_caps_unref(priv->filter);
+
+  (*parent_class->finalize) (object);
+  return;
+}
+
+PipelineVideo *
+cheese_pipeline_video_new(void)
+{
+  PipelineVideo *self = g_object_new(PIPELINE_VIDEO_TYPE, NULL);
+
+  return self;
+}
+
+void
+cheese_pipeline_video_class_init(PipelineVideoClass *klass)
+{
+  GObjectClass *object_class;
+
+  parent_class = g_type_class_peek_parent(klass);
+  object_class = (GObjectClass*) klass;
+
+  object_class->finalize = cheese_pipeline_video_finalize;
+  g_type_class_add_private(klass, sizeof(PipelineVideoPrivate));
+
+  G_OBJECT_CLASS(klass)->finalize = (GObjectFinalizeFunc) cheese_pipeline_video_finalize;
+}
+
+void
+cheese_pipeline_video_init(PipelineVideo *self)
+{
+}
+
+void
+cheese_pipeline_video_set_play(PipelineVideo *self)
+{
+  gst_element_set_state(self->pipeline, GST_STATE_PLAYING);
+  return;
+}
+
+void
+cheese_pipeline_video_set_stop(PipelineVideo *self)
+{
+  gst_element_set_state(self->pipeline, GST_STATE_NULL);
+  return;
+}
+
+void
+cheese_pipeline_video_button_clicked(GtkWidget *widget, gpointer self)
+{
+  cheese_pipeline_video_lens_open(self);
+  return;
+}
+
+static void
+cheese_pipeline_video_lens_open(PipelineVideo *self)
+{
+  PipelineVideoPrivate *priv = PIPELINE_VIDEO_GET_PRIVATE(self);
+  priv->picture_requested = TRUE;
+  return;
+}
+
+GstElement *
+cheese_pipeline_video_get_ximagesink(PipelineVideo *self)
+{
+  return self->ximagesink;
+}
+
+GstElement *
+cheese_pipeline_video_get_fakesink(PipelineVideo *self)
+{
+  return self->fakesink;
+}
+
+GstElement *
+cheese_pipeline_video_get_pipeline(PipelineVideo *self)
+{
+  return self->pipeline;
+}
+
+void
+cheese_pipeline_video_change_effect(gchar *effect, gpointer self)
+{
+  PipelineVideoPrivate *priv = PIPELINE_VIDEO_GET_PRIVATE(self);
+
+  if (effect != NULL) {
+    cheese_pipeline_video_set_stop(PIPELINE_VIDEO(self));
+
+    gst_element_unlink(priv->ffmpeg1, priv->effect);
+    gst_element_unlink(priv->effect, priv->ffmpeg2);
+    gst_bin_remove(GST_BIN(PIPELINE_VIDEO(self)->pipeline), priv->effect);
+
+    g_print("changing to effect: %s\n", effect);
+    priv->effect = gst_parse_bin_from_description(effect, TRUE, NULL);
+    gst_bin_add(GST_BIN(PIPELINE_VIDEO(self)->pipeline), priv->effect);
+
+    gst_element_link(priv->ffmpeg1, priv->effect);
+    gst_element_link(priv->effect, priv->ffmpeg2);
+
+    cheese_pipeline_video_set_play(self);
+  }
+}
+
+void
+cheese_pipeline_video_create(gchar *source_pipeline, PipelineVideo *self) {
+
+  PipelineVideoPrivate *priv = PIPELINE_VIDEO_GET_PRIVATE(self);
+  gboolean link_ok; 
+
+  self->pipeline = gst_pipeline_new("pipeline");
+  priv->source = gst_parse_bin_from_description(source_pipeline, TRUE, NULL);
+  //priv->source = gst_element_factory_make(source_pipeline, source_pipeline);
+  // if you want to test without having a webcam
+  // priv->source = gst_element_factory_make("videotestsrc", "v4l2src");
+  gst_bin_add(GST_BIN(self->pipeline), priv->source);
+
+  priv->ffmpeg1 = gst_element_factory_make("ffmpegcolorspace", "ffmpegcolorspace");
+  gst_bin_add(GST_BIN(self->pipeline), priv->ffmpeg1);
+
+  priv->effect = gst_element_factory_make("identity", "effect");
+  gst_bin_add(GST_BIN(self->pipeline), priv->effect);
+
+  priv->ffmpeg2 = gst_element_factory_make("ffmpegcolorspace", "ffmpegcolorspace2");
+  gst_bin_add(GST_BIN(self->pipeline), priv->ffmpeg2);
+
+  priv->ffmpeg3 = gst_element_factory_make("ffmpegcolorspace", "ffmpegcolorspace3");
+  gst_bin_add(GST_BIN(self->pipeline), priv->ffmpeg3);
+
+  priv->tee = gst_element_factory_make("tee", "tee");
+  gst_bin_add(GST_BIN(self->pipeline), priv->tee);
+
+  priv->queuevid = gst_element_factory_make("queue", "queuevid");
+  gst_bin_add(GST_BIN(self->pipeline), priv->queuevid);
+
+  priv->queueimg = gst_element_factory_make("queue", "queueimg");
+  gst_bin_add(GST_BIN(self->pipeline), priv->queueimg);
+
+  self->ximagesink = gst_element_factory_make("gconfvideosink", "gconfvideosink");
+  gst_bin_add(GST_BIN(self->pipeline), self->ximagesink);
+
+  self->fakesink = gst_element_factory_make("fakesink", "fakesink");
+  gst_bin_add(GST_BIN(self->pipeline), self->fakesink);
+
+  /*
+   * the pipeline looks like this:
+   * gconfvideosrc -> ffmpegcsp
+   *                    '-> videoscale
+   *                         '-> ffmpegcsp -> effects -> ffmpegcsp 
+   *    -------------------------------------------------------'
+   *    '--> tee (filtered) -> queue-> ffmpegcsp -> gconfvideosink
+   *          |
+   *       queueimg -> fakesink -> pixbuf (gets picture from data)
+   */
+
+
+  gst_element_link(priv->source, priv->ffmpeg1);
+
+  gst_element_link(priv->ffmpeg1, priv->effect);
+  gst_element_link(priv->effect, priv->ffmpeg2);
+
+  priv->filter = gst_caps_new_simple("video/x-raw-rgb",
+      "bpp", G_TYPE_INT, 24,
+      "depth", G_TYPE_INT, 24, NULL);
+
+  link_ok = gst_element_link_filtered(priv->ffmpeg2, priv->tee, priv->filter);
+  if (!link_ok) {
+    g_warning("Failed to link elements!");
+  }
+
+  gst_element_link(priv->tee, priv->queuevid);
+  gst_element_link(priv->queuevid, priv->ffmpeg3);
+
+  gst_element_link(priv->ffmpeg3, self->ximagesink);
+
+  // setting back the format to get nice pictures
+  priv->filter = gst_caps_new_simple("video/x-raw-rgb", NULL);
+  link_ok = gst_element_link_filtered(priv->tee, priv->queueimg, priv->filter);
+  if (!link_ok) {
+    g_warning("Failed to link elements!");
+  }
+  //gst_element_link(priv->tee, priv->queueimg);
+
+  gst_element_link(priv->queueimg, self->fakesink);
+  g_object_set(G_OBJECT(self->fakesink), "signal-handoffs", TRUE, NULL);
+
+  g_signal_connect(G_OBJECT(self->fakesink), "handoff", 
+                   G_CALLBACK(cheese_pipeline_video_have_data_cb), self);
+
+}
+static gboolean
+cheese_pipeline_video_have_data_cb(GstElement *element, GstBuffer *buffer, GstPad *pad, gpointer self)
+{
+  PipelineVideoPrivate *priv = PIPELINE_VIDEO_GET_PRIVATE(self);
+
+  unsigned char *data_video = (unsigned char *)GST_BUFFER_DATA(buffer);
+  if (priv->picture_requested) {
+    GstCaps *caps;
+    const GstStructure *structure;
+
+    int width, height;
+
+    caps = gst_buffer_get_caps(buffer);
+    structure = gst_caps_get_structure(caps, 0);
+    gst_structure_get_int(structure, "width", &width);
+    gst_structure_get_int(structure, "height", &height);
+
+    cheese_pipeline_video_create_video(data_video, width, height);
+    priv->picture_requested = FALSE;
+  }
+  return TRUE;
+}
+
+void
+cheese_pipeline_video_create_video(unsigned char *data, int width, int height)
+{
+  int i;
+  gchar *filename = NULL;
+  GnomeVFSURI *uri;
+  GdkPixbuf *pixbuf;
+
+  i = 1;
+  //FIXME: get the video name
+  filename = cheese_fileutil_get_photo_filename(i);
+
+  uri = gnome_vfs_uri_new(filename);
+
+  while (gnome_vfs_uri_exists(uri)) {
+    i++;
+  //FIXME: get the video name
+    filename = cheese_fileutil_get_photo_filename(i);
+    g_free(uri);
+    uri = gnome_vfs_uri_new(filename);
+  }
+  g_free(uri);
+
+  pixbuf = gdk_pixbuf_new_from_data (data, GDK_COLORSPACE_RGB, FALSE, 8, 
+                                     width, height, width * 3, NULL, NULL);
+
+  gdk_pixbuf_save(pixbuf, filename, "jpeg", NULL, NULL);
+  g_object_unref(G_OBJECT(pixbuf));
+
+  g_print("Video saved: %s (%dx%d)\n", filename, width, height);
+}
---
+++ src/cheese-pipeline-video.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2007 Copyright (C) 2007 daniel g. siegel <dgsiegel@gmail.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CHEESE_PIPELINE_VIDEO_H__
+#define __CHEESE_PIPELINE_VIDEO_H__
+#include <glib.h>
+#include <glib-object.h>
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define PIPELINE_VIDEO_TYPE             (cheese_pipeline_video_get_type())
+#define PIPELINE_VIDEO(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), PIPELINE_VIDEO_TYPE, PipelineVideo))
+#define PIPELINE_VIDEO_CLASS(vtable)    (G_TYPE_CHECK_CLASS_CAST ((vtable), PIPELINE_VIDEO_TYPE, PipelineVideoClass))
+#define IS_PIPELINE_VIDEO(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PIPELINE_VIDEO_TYPE))
+#define IS_PIPELINE_VIDEO_CLASS(vtable) (G_TYPE_CHECK_CLASS_TYPE ((vtable), PIPELINE_VIDEO_TYPE))
+#define PIPELINE_VIDEO_GET_CLASS(inst)  (G_TYPE_INSTANCE_GET_CLASS ((inst), PIPELINE_VIDEO_TYPE, PipelineVideoClass))
+
+typedef struct _PipelineVideo PipelineVideo;
+typedef struct _PipelineVideoClass PipelineVideoClass;
+
+struct _PipelineVideo 
+{
+	GObject parent;
+
+  GstElement *pipeline;
+  GstElement *ximagesink;
+  GstElement *fakesink;
+};
+
+struct _PipelineVideoClass 
+{
+	GObjectClass parent_class;
+};
+
+PipelineVideo*    cheese_pipeline_video_new               (void);
+GType             cheese_pipeline_video_get_type          (void);
+void              cheese_pipeline_video_set_play          (PipelineVideo *self);
+void              cheese_pipeline_video_set_stop          (PipelineVideo *self);
+void              cheese_pipeline_video_create            (gchar *source_pipeline, PipelineVideo *self);
+GstElement       *cheese_pipeline_video_get_ximagesink    (PipelineVideo *self);
+GstElement       *cheese_pipeline_video_get_fakesink      (PipelineVideo *self);
+GstElement       *cheese_pipeline_video_get_pipeline      (PipelineVideo *self);
+void              cheese_pipeline_video_button_clicked    (GtkWidget *widget, gpointer self);
+void              cheese_pipeline_video_change_effect     (gchar *effect, gpointer self);
+
+G_END_DECLS
+
+
+#endif /* __CHEESE_PIPELINE_VIDEO_H__ */
