539af9fc3039fbeec0fd09d8d09b3ccff13e6918
Jaap A. Haitsma <jhaitsma@src.gnome.org>
2007-12-04 21:07:09 +0000

    Add boilerplate for manual in cheese. Still need to add waf support. Going to add that now A help/cheese.omf.in A help/C A help/C/legal.xml A help/C/figures A help/C/figures/cheese-screenshot.jpg A help/C/cheese.xml A help/ChangeLog A help/Makefile.am A help/C/legal.xml A help/C/figures A help/C/figures/cheese-screenshot.jpg A help/C/cheese.xml A help/C/figures/cheese-screenshot.jpg
    svn path=/trunk/; revision=321

--- src/cheese-window.c
+++ src/cheese-window.c
@@ -480,6 +480,56 @@ cheese_window_cmd_command_line (GtkAction *action, CheeseWindow *cheese_window)
 
 
 static void
+cheese_window_cmd_help_contents (GtkAction *action, CheeseWindow *cheese_window)
+{
+  GError *error = NULL;
+  char *command;
+  const char *lang;
+  char *uri = NULL;
+  GdkScreen *gscreen;
+  // TODO what do we want for link_id
+  char *link_id = NULL;
+
+  int i;
+
+  const char * const * langs = g_get_language_names ();
+
+  for (i = 0; langs[i]; i++) 
+  {
+    lang = langs[i];
+    if (strchr (lang, '.')) 
+      continue;
+
+    uri = g_build_filename(DATADIR, "/gnome/help/cheese/", lang, "/cheese.xml", NULL);
+					
+    if (g_file_test (uri, G_FILE_TEST_EXISTS))
+      break;
+  }
+	
+  if (link_id)
+    command = g_strconcat ("gnome-open ghelp://", uri, "?", link_id, NULL);
+  else
+    command = g_strconcat ("gnome-open ghelp://", uri,  NULL);
+	
+
+  gscreen = gdk_screen_get_default();
+  gdk_spawn_command_line_on_screen (gscreen, command, &error);
+  if (error != NULL) 
+  {
+    GtkWidget *d;
+
+    d = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+			       GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, error->message);
+    gtk_dialog_run(GTK_DIALOG(d));
+    gtk_widget_destroy(d);
+    g_error_free(error);
+  }
+
+  g_free (command);
+  g_free (uri);
+}
+
+static void
 cheese_window_cmd_about (GtkAction *action, CheeseWindow *cheese_window)
 {
   static const char *authors[] = {
@@ -728,7 +778,9 @@ static const GtkActionEntry action_entries_main[] = {
   {"Help", NULL, N_("_Help")},
 
   {"Quit", GTK_STOCK_QUIT, NULL, NULL, NULL, G_CALLBACK (cheese_window_cmd_close)},
+  {"HelpContents", GTK_STOCK_HELP, N_("_Contents"), "F1", N_("Help on this application"), G_CALLBACK (cheese_window_cmd_help_contents)},
   {"About", GTK_STOCK_ABOUT, NULL, NULL, NULL, G_CALLBACK (cheese_window_cmd_about)},
+
 };
 
 static const GtkActionEntry action_entries_edit_file[] = {
