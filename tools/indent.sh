eb2f25fb13735644156d0f13dd405b6361ec5004
Daniel G. Siegel <dgsiegel@src.gnome.org>
2008-08-22 16:07:44 +0000

    add the uncrustify indenting config
    svn path=/trunk/; revision=951

---
+++ tools/indent.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+INDENT_PROGRAM="uncrustify"
+DIR="tools"
+CFG="cheese-indent.cfg"
+LANG="C"
+
+[[ $# -lt 1 ]] && { echo "$0 [files]" 1>&2; exit 1; }
+
+if ! which $INDENT_PROGRAM > /dev/null 2>&1; then
+  echo "$INDENT_PROGRAM was not found on your computer, please install it"
+  exit 1
+fi
+
+$INDENT_PROGRAM -l $LANG -c $DIR/$CFG --no-backup --replace $@
