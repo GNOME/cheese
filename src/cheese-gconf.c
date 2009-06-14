7714c30c506e11545d38180f62d044025796cdd4
Filippo Argiolas <filippo.argiolas@gmail.com>
2009-06-14 11:59:18 +0200

    gconf: fix a memory leak
    g_strjoin allocates a new string, so the old effect string was leaking.

--- src/cheese-gconf.c
+++ src/cheese-gconf.c
@@ -92,8 +92,11 @@ cheese_gconf_get_property (GObject *object, guint prop_id, GValue *value,
         }
         if (effects == NULL)
           effects = g_strdup (str);
-        else
+        else {
+          gchar *dummy = effects;
           effects = g_strjoin (",", effects, str, NULL);
+          g_free (dummy);
+        }
 
         g_free (tmp->data);
         tmp = g_slist_next (tmp);
