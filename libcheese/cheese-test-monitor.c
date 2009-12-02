a4f10acf3d3386478408bc1965e400199220425e
Bastien Nocera <hadess@hadess.net>
2009-12-02 18:00:46 +0000

    Add test application for device monitor

---
+++ libcheese/cheese-test-monitor.c
@@ -0,0 +1,35 @@
+#include <glib.h>
+
+#include "cheese-camera-device-monitor.h"
+
+static void
+added_cb (CheeseCameraDeviceMonitor *monitor,
+	  CheeseCameraDevice *device)
+{
+	g_message ("Added new device with ID '%s'", device->hal_udi);
+}
+
+static void
+removed_cb (CheeseCameraDeviceMonitor *monitor,
+	    const char *id)
+{
+	g_message ("Removed device with ID '%s'", id);
+}
+
+int main (int argc, char **argv)
+{
+	CheeseCameraDeviceMonitor *monitor;
+
+	g_type_init ();
+
+	monitor = cheese_camera_device_monitor_new ();
+	g_signal_connect (G_OBJECT (monitor), "added",
+			  G_CALLBACK (added_cb), NULL);
+	g_signal_connect (G_OBJECT (monitor), "removed",
+			  G_CALLBACK (removed_cb), NULL);
+	cheese_camera_device_monitor_coldplug (monitor);
+
+	g_main_loop_run (g_main_loop_new (NULL, FALSE));
+
+	return 0;
+}
