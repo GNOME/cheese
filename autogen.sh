807600a71748a15aa72bce4d7d5344a6e59e18a7
Javier Jardón <jjardon@gnome.org>
2011-02-06 23:21:26 +0000

    build: Update autotools configuration

--- autogen.sh
+++ autogen.sh
@@ -14,7 +14,7 @@ REQUIRED_VALA_VERSION=0.9.2
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common"
     exit 1
 }
 
@@ -53,10 +53,4 @@ if ! compare_versions $REQUIRED_VALA_VERSION $VALA_VERSION; then
     exit 1
 fi
 
-gtkdocize || exit 1
-
-REQUIRED_AUTOCONF_VERSION=2.57
-REQUIRED_AUTOMAKE_VERSION=1.7
-REQUIRED_INTLTOOL_VERSION=0.40.0
-REQUIRED_PKG_CONFIG_VERSION=0.16.0
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+. gnome-autogen.sh
