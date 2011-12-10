ea8aa26a7bccc5a6e8d3d5242c0b74596c5ddca2
David King <amigadave@amigadave.com>
2011-12-10 23:34:31 +0100

    Simplify autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="cheese"
-REQUIRED_VALA_VERSION=0.9.2
 
 (test -f $srcdir/src/cheese-main.vala) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
@@ -18,39 +17,4 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-# Usage:
-#     compare_versions MIN_VERSION ACTUAL_VERSION
-# returns true if ACTUAL_VERSION >= MIN_VERSION
-compare_versions() {
-    ch_min_version=$1
-    ch_actual_version=$2
-    ch_status=0
-    IFS="${IFS=         }"; ch_save_IFS="$IFS"; IFS="."
-    set $ch_actual_version
-    for ch_min in $ch_min_version; do
-        ch_cur=`echo $1 | sed 's/[^0-9].*$//'`; shift # remove letter suffixes
-        if [ -z "$ch_min" ]; then break; fi
-        if [ -z "$ch_cur" ]; then ch_status=1; break; fi
-        if [ $ch_cur -gt $ch_min ]; then break; fi
-        if [ $ch_cur -lt $ch_min ]; then ch_status=1; break; fi
-    done
-    IFS="$ch_save_IFS"
-    return $ch_status
-}
-
-which valac || {
-    echo "**Error**: You must have valac >= $REQUIRED_VALA_VERSION installed to build $PKG_NAME"
-    exit 1
-}
-
-# Vala version check
-test -z "$VALAC" && VALAC=valac
-VALA_VERSION=`$VALAC --version | cut -d" " -f2`
-echo $VALA_VERSION
-
-if ! compare_versions $REQUIRED_VALA_VERSION $VALA_VERSION; then
-    echo "**Error**: You must have valac >= $REQUIRED_VALA_VERSION installed to build $PKG_NAME, you have $VALA_VERSION"
-    exit 1
-fi
-
-. gnome-autogen.sh
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS" USE_GNOME2_MACROS=1 . gnome-autogen.sh
