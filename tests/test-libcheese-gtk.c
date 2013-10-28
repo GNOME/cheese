e6f75ed1023e6d3cf2d67ebb3d34c54d706a03fc
David King <amigadave@amigadave.com>
2013-10-28 22:06:40 +0000

    Fix CheeseAvatarChooser test
    There is no longer a button with the label "Take a Photo", so remove the
    test which tries to find it.

--- tests/test-libcheese-gtk.c
+++ tests/test-libcheese-gtk.c
@@ -41,8 +41,6 @@ avatar_chooser (void)
     g_assert (chooser != NULL);
 
     /* Check that all the child widgets were successfully instantiated. */
-    g_assert (gtk_test_find_widget (chooser, "Take a Photo", GTK_TYPE_BUTTON)
-        != NULL);
     select_button = gtk_test_find_widget (chooser, "Select", GTK_TYPE_BUTTON);
     g_assert (select_button != NULL);
     g_assert (GTK_IS_BUTTON (select_button));
