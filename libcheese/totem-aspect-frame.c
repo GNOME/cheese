f98161345cee70569a986f3ef7344ed08883e506
Dominique Leuenberger <dimstar@opensuse.org>
2014-01-08 21:21:25 +0100

    Update FSF Address
    https://bugzilla.gnome.org/show_bug.cgi?id=721536

--- libcheese/totem-aspect-frame.c
+++ libcheese/totem-aspect-frame.c
@@ -19,7 +19,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- * Boston, MA 02111-1307, USA.
  */
 
 #include <math.h>
--- libcheese/totem-aspect-frame.h
+++ libcheese/totem-aspect-frame.h
@@ -17,8 +17,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- * Boston, MA 02111-1307, USA.
- *
  */
 
 #ifndef __TOTEM_ASPECT_FRAME_H__
