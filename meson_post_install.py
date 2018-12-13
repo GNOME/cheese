77f351123ae70b25f8fb942a7f98bd45775e18d1
Iñigo Martínez <inigomartinez@gmail.com>
2018-12-13 18:09:28 +0100

    build: Port to meson build system
    meson is a build system focused on speed an ease of use, which
    helps speeding up the software development. This patch adds meson
    support along autotools.

---
+++ meson_post_install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+if not os.environ.get('DESTDIR'):
+    prefix = os.environ['MESON_INSTALL_PREFIX']
+
+    icondir = os.path.join(prefix, sys.argv[1], 'icons', 'hicolor')
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
+
+    schemasdir = os.path.join(prefix, sys.argv[2])
+    print('Compiling gsettings schemas...')
+    subprocess.call(['glib-compile-schemas', schemasdir])
