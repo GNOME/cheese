GNOME/cheese

commit 9f0825c3169047da75d3fe91a3cb02bfe6fb7f25

Allow device parameter to understand api.v4l2.path
https://gitlab.gnome.org/GNOME/cheese/-/merge_requests/37
https://gitlab.gnome.org/GNOME/cheese/-/issues/4

libcheese/cheese-camera-device.c
@@ -87,6 +87,7 @@ enum
 {
   PROP_0,
   PROP_NAME,
+  PROP_PATH,
   PROP_DEVICE,
   PROP_LAST
 };
@@ -99,6 +100,7 @@ typedef struct
   gchar     *name;
   GstCaps   *caps;
   GList     *formats; /* list members are CheeseVideoFormatFull structs. */
+  gchar     *path;
 
   GError    *construct_error;
 } CheeseCameraDevicePrivate;
@@ -602,6 +604,9 @@ cheese_camera_device_get_property (GObject *object, guint prop_id, GValue *value
     case PROP_DEVICE:
       g_value_set_object (value, priv->device);
       break;
+    case PROP_PATH:
+      g_value_set_string (value, priv->path);
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -626,6 +631,12 @@ cheese_camera_device_set_property (GObject *object, guint prop_id, const GValue 
       priv->device = g_value_dup_object (value);
       g_free (priv->name);
       priv->name = gst_device_get_display_name (priv->device);
+      g_free (priv->path);
+      priv->path = g_value_dup_string (gst_structure_get_value (gst_device_get_properties (priv->device), "api.v4l2.path"));
+      break;
+    case PROP_PATH:
+      g_free (priv->path);
+      priv->path = g_value_dup_string (value);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -641,6 +652,7 @@ cheese_camera_device_finalize (GObject *object)
 
   g_object_unref (priv->device);
   g_free (priv->name);
+  g_free (priv->path);
 
   gst_caps_unref (priv->caps);
   free_format_list (device);
@@ -678,6 +690,18 @@ cheese_camera_device_class_init (CheeseCameraDeviceClass *klass)
                                                G_PARAM_CONSTRUCT_ONLY |
                                                G_PARAM_STATIC_STRINGS);
 
+  /**
+   * CheeseCameraDevice:path:
+   *
+   * Path of the video capture device.
+   */
+  properties[PROP_PATH] = g_param_spec_string ("path",
+                                               "Device path",
+                                               "Path of the video capture device",
+                                               NULL,
+                                               G_PARAM_READWRITE |
+                                               G_PARAM_CONSTRUCT_ONLY |
+                                               G_PARAM_STATIC_STRINGS);
   /**
    * CheeseCameraDevice:device:
    *
@@ -781,6 +805,25 @@ cheese_camera_device_get_format_list (CheeseCameraDevice *device)
     return g_list_copy (priv->formats);
 }
 
+/**
+ * cheese_camera_device_get_path:
+ * @device: a #CheeseCameraDevice
+ *
+ * Get path for the device, as reported by udev.
+ *
+ * Returns: (transfer none): the path of the video capture device
+ */
+const gchar *
+cheese_camera_device_get_path (CheeseCameraDevice *device)
+{
+  CheeseCameraDevicePrivate *priv;
+
+  g_return_val_if_fail (CHEESE_IS_CAMERA_DEVICE (device), NULL);
+
+  priv = cheese_camera_device_get_instance_private (device);
+
+  return priv->path;
+}
 /**
  * cheese_camera_device_get_name:
  * @device: a #CheeseCameraDevice

libcheese/cheese-camera-device.h
@@ -73,6 +73,7 @@ CheeseVideoFormat *cheese_camera_device_get_best_format (CheeseCameraDevice *dev
 GList *            cheese_camera_device_get_format_list (CheeseCameraDevice *device);
 
 const gchar *cheese_camera_device_get_name (CheeseCameraDevice *device);
+const gchar *cheese_camera_device_get_path (CheeseCameraDevice *device);
 GstElement * cheese_camera_device_get_src (CheeseCameraDevice *device);
 
 GstCaps * cheese_camera_device_supported_format_caps (void);

libcheese/cheese-camera.c
@@ -1594,8 +1594,8 @@ cheese_camera_setup (CheeseCamera *camera, CheeseCameraDevice *device, GError **
     {
       device = g_ptr_array_index (priv->camera_devices, i);
 
-      if (g_strcmp0 (cheese_camera_device_get_name (device),
-                     priv->initial_name) == 0)
+      if (g_strcmp0 (cheese_camera_device_get_name (device), priv->initial_name) == 0
+          || g_strcmp0 (cheese_camera_device_get_path (device), priv->initial_name) == 0)
       {
         cheese_camera_set_device (camera, device);
         break;

src/vapi/cheese-common.vapi
@@ -78,11 +78,14 @@ namespace Cheese
     public Gst.Caps get_caps_for_format (Cheese.VideoFormat format);
     public GLib.List<unowned Cheese.VideoFormat> get_format_list ();
     public unowned string             get_name ();
+    public unowned string             get_path ();
     public Gst.Element                get_src ();
     [NoAccessorMethod]
     public Gst.Device device {get; construct;}
     [NoAccessorMethod]
     public string name {get;}
+    [NoAccessorMethod]
+    public string path {get;}
   }
 
   [CCode (cheader_filename = "cheese-camera-device-monitor.h")]

commit ddee436de9acb758ca588a326b66cf26cee41e50

Fix warning when v4l device doesn't have a path
 #0  0x00007ffff7e4cf77 in g_logv () at /lib64/libglib-2.0.so.0
 #1  0x00007ffff7e4d233 in g_log () at /lib64/libglib-2.0.so.0
 #2  0x00007ffff7f6b37b in g_value_dup_string () at /lib64/libgobject-2.0.so.0
 #3  0x00007ffff7fb601c in cheese_camera_device_set_property (object=0x6c4360, prop_id=3, value=0x7fffffffc6a0, pspec=0x6c6a40) at ../../../../Projects/jhbuild/cheese/libcheese/cheese-camera-device.c:635
 #4  0x00007ffff7f50a46 in object_set_property () at /lib64/libgobject-2.0.so.0
 #5  0x00007ffff7f50e14 in g_object_new_internal () at /lib64/libgobject-2.0.so.0
 #6  0x00007ffff7f522c8 in g_object_new_valist () at /lib64/libgobject-2.0.so.0
 #7  0x00007ffff7a5921e in g_initable_new_valist () at /lib64/libgio-2.0.so.0
 #8  0x00007ffff7a592ed in g_initable_new () at /lib64/libgio-2.0.so.0
 #9  0x00007ffff7fb674e in cheese_camera_device_new (device=0x7fffec0058c0, error=0x7fffffffcc28) at ../../../../Projects/jhbuild/cheese/libcheese/cheese-camera-device.c:781
 #10 0x00007ffff7fb6e68 in cheese_camera_device_monitor_set_up_device (device=0x7fffec0058c0) at ../../../../Projects/jhbuild/cheese/libcheese/cheese-camera-device-monitor.c:96
 #11 cheese_camera_device_monitor_added (monitor=0x6c7c20, device=0x7fffec0058c0) at ../../../../Projects/jhbuild/cheese/libcheese/cheese-camera-device-monitor.c:117
 #12 0x00007ffff7fb7058 in cheese_camera_device_monitor_add_devices (data=0x7fffec0058c0, user_data=<optimized out>) at ../../../../Projects/jhbuild/cheese/libcheese/cheese-camera-device-monitor.c:189
 #13 0x00007ffff7e42840 in g_list_foreach () at /lib64/libglib-2.0.so.0
 #14 0x00007ffff7fb7311 in cheese_camera_device_monitor_coldplug (monitor=0x6c7c20) at ../../../../Projects/jhbuild/cheese/libcheese/cheese-camera-device-monitor.c:221
 #15 0x0000000000401d4f in cameradevicemonitor_create () at ../../../../Projects/jhbuild/cheese/tests/test-libcheese.c:39
 #16 0x00007ffff7e71bd6 in g_test_run_suite_internal () at /lib64/libglib-2.0.so.0
 #17 0x00007ffff7e718fb in g_test_run_suite_internal () at /lib64/libglib-2.0.so.0
 #18 0x00007ffff7e718fb in g_test_run_suite_internal () at /lib64/libglib-2.0.so.0
 #19 0x00007ffff7e720aa in g_test_run_suite () at /lib64/libglib-2.0.so.0
 #20 0x00007ffff7e720d1 in g_test_run () at /lib64/libglib-2.0.so.0
 #21 0x00000000004012b8 in main (argc=<optimized out>, argv=<optimized out>) at ../../../../Projects/jhbuild/cheese/tests/test-libcheese.c:231

The api.v4l2.path might not always be available, so avoid using it when
not present.

gst-device-monitor-1.0 on my system:

	name  : USB2.0 FHD UVC WebCam: USB2.0 F
	class : Video/Source
	caps  : video/x-raw, format=YUY2, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1
	        video/x-raw, format=YUY2, width=640, height=360, pixel-aspect-ratio=1/1, framerate=30/1
	        video/x-raw, format=YUY2, width=352, height=288, pixel-aspect-ratio=1/1, framerate=30/1
	        video/x-raw, format=YUY2, width=320, height=240, pixel-aspect-ratio=1/1, framerate=30/1
	        video/x-raw, format=YUY2, width=176, height=144, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=1920, height=1080, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=1280, height=720, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=848, height=480, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=640, height=360, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=352, height=288, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=320, height=240, pixel-aspect-ratio=1/1, framerate=30/1
	        image/jpeg, width=176, height=144, pixel-aspect-ratio=1/1, framerate=30/1
	properties:
		udev-probed = true
		device.bus_path = pci-0000:00:14.0-usb-0:1.4:1.0
		sysfs.path = /sys/devices/pci0000:00/0000:00:14.0/usb1/1-1/1-1.4/1-1.4:1.0/video4linux/video0
		device.bus = usb
		device.subsystem = video4linux
		device.vendor.id = 04f2
		device.vendor.name = "SunplusIT\\x20Inc"
		device.product.id = b612
		device.product.name = "USB2.0\ FHD\ UVC\ WebCam:\ USB2.0\ F"
		device.serial = SunplusIT_Inc_USB2.0_FHD_UVC_WebCam_01.00.00
		device.capabilities = :capture:
		device.api = v4l2
		device.path = /dev/video0
		v4l2.device.driver = uvcvideo
		v4l2.device.card = "USB2.0\ FHD\ UVC\ WebCam:\ USB2.0\ F"
		v4l2.device.bus_info = usb-0000:00:14.0-1.4
		v4l2.device.version = 331538 (0x00050f12)
		v4l2.device.capabilities = 2225078273 (0x84a00001)
		v4l2.device.device_caps = 69206017 (0x04200001)
	gst-launch-1.0 v4l2src ! ...

Fixes: 9f0825c3169047da75d3fe91a3cb02bfe6fb7f25

libcheese/cheese-camera-device.c
@@ -618,6 +618,7 @@ cheese_camera_device_set_property (GObject *object, guint prop_id, const GValue 
 {
   CheeseCameraDevice        *device = CHEESE_CAMERA_DEVICE (object);
   CheeseCameraDevicePrivate *priv = cheese_camera_device_get_instance_private (device);
+  const GValue *tmp;
 
   switch (prop_id)
   {
@@ -626,13 +627,15 @@ cheese_camera_device_set_property (GObject *object, guint prop_id, const GValue 
       priv->name = g_value_dup_string (value);
       break;
     case PROP_DEVICE:
-      if (priv->device)
-        g_object_unref (priv->device);
+      g_clear_object (&priv->device);
       priv->device = g_value_dup_object (value);
       g_free (priv->name);
       priv->name = gst_device_get_display_name (priv->device);
-      g_free (priv->path);
-      priv->path = g_value_dup_string (gst_structure_get_value (gst_device_get_properties (priv->device), "api.v4l2.path"));
+      tmp = gst_structure_get_value (gst_device_get_properties (priv->device), "api.v4l2.path");
+      if (tmp) {
+        g_clear_pointer (&priv->path, g_free);
+        priv->path = g_value_dup_string (tmp);
+      }
       break;
     case PROP_PATH:
       g_free (priv->path);